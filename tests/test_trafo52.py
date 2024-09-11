01db695a8eaede832b39e5cf73b71cd87c1f7137
Julian Neuberger <neuberger.julian@gmail.com>
2024-09-11 13:52:00 +0200

    adds examples for select trafos

--- tests/test_trafo52.py
+++
@@ -1,7 +0,0 @@
-from augment import trafo52
-
-
-def test_load():
-    abbreviations = trafo52.InsertAbbreviations._load()
-
-    assert len(abbreviations) > 0
--- tests/test_trafo82.py
+++
@@ -1,7 +0,0 @@
-from augment import trafo82
-
-
-def test_load():
-    abbreviations = trafo82.Trafo82Step._load()
-
-    assert len(abbreviations) > 0
